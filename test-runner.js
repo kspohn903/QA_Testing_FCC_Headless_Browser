2591190c4c9d4220538a4c3fd9818a3956bde06b
kspohn903 <mlpfanatic903@gmail.com>
2022-10-23 11:04:48 -0500

    Update test-runner.js

--- test-runner.js
+++ test-runner.js
@@ -6,18 +6,13 @@ const Mocha = require('mocha'),
     path = require('path');
 
 const mocha = new Mocha();
-const testDir = './tests'
+const testDir = './tests'; // Add each .js file to the mocha instance
 
-
-// Add each .js file to the mocha instance
-fs.readdirSync(testDir).filter(function (file) {
-    // Only keep the .js files
-    return file.substr(-3) === '.js';
-
-}).forEach(function (file) {
-    mocha.addFile(
-        path.join(testDir, file)
-    );
+fs.readdirSync(testDir).filter((file) => { 
+    return file.substr(-3) === '.js'; 
+    //Only keep the .js files
+}).forEach( (file) => {
+    mocha.addFile(path.join(testDir, file) );
 });
 
 const emitter = new EventEmitter();
@@ -28,12 +23,10 @@ emitter.run = function () {
     const separator = ' -> ';
     // Run the tests.
     try {
-        const runner = mocha.ui('tdd').run()
-            .on('test end', function (test) {
-                // remove comments
-                let body = test.body.replace(/\/\/.*\n|\/\*.*\*\//g, '');
-                // collapse spaces
-                body = body.replace(/\s+/g, ' ');
+        const runner = mocha.ui('tdd').run().on('test end', 
+             (test) => {
+                let body = test.body.replace(/\/\/.*\n|\/\*.*\*\//g, '');  // remove comments
+                body = body.replace(/\s+/g, ' '); // collapse spaces
                 const obj = {
                     title: test.title,
                     context: context.slice(0, -separator.length),
@@ -42,18 +35,14 @@ emitter.run = function () {
                     assertions: analyser(body)
                 };
                 tests.push(obj);
-            })
-            .on('end', function () {
+            }).on('end', () => {
                 emitter.report = tests;
-                emitter.emit('done', tests)
-            })
-            .on('suite', function (s) {
+                emitter.emit('done', tests);
+            }).on('suite',(s) => {
                 context += (s.title + separator);
-
-            })
-            .on('suite end', function (s) {
-                context = context.slice(0, -(s.title.length + separator.length))
-            })
+            }).on('suite end', (s) => {
+                context = context.slice(0, -(s.title.length + separator.length));
+            });
     } catch (e) {
         throw (e);
     }
